13d7d0af91cc61962cc7c611fd50a302938417fc
JAZMIN_LOPEZ_MARTINEZ <a.21308051280495@cbtis128.edu.mx>
2024-05-07 14:12:09 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Container_0495",
+      home: const MyHomePage(title: 'Container_0495'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(width: 5, color: Colors.red),
+            boxShadow: [
+              const BoxShadow(
+                  color: Colors.blue,
+                  offset: Offset(5.0, 5.0),
+                  blurRadius: 20.0,
+                  spreadRadius: 10.0),
+              const BoxShadow(
+                  color: Colors.green,
+                  offset: Offset(2.0, 5.0),
+                  blurRadius: 10.0,
+                  spreadRadius: 5.0),
+            ],
+          ),
+          width: 250,
+          height: 300,
+          child: const Text(
+            "Soy un contenedor",
+            style: TextStyle(fontSize: 28, color: Colors.white),
+          ),
+          constraints: const BoxConstraints(
+            minHeight: 100,
+            minWidth: 100,
+            maxHeight: double.infinity,
+            maxWidth: double.infinity,
+          ),
+          transform: Matrix4.skew(0.2, -0.2)..rotateZ(0.9),
+          transformAlignment: Alignment.center,
         ),
       ),
     );
